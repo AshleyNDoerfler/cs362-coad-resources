192c97a74d4634fce902ec9f2164635339865e7a
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-20 16:17:31 -0800

    organization has a name, status, ..., transportation tests all passed

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,4 +6,64 @@ RSpec.describe Organization, type: :model do
     Organization.new
   end
 
+  it "has a name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:name)
+  end
+
+  it "has a status" do
+    organization = Organization.new
+    expect(organization).to respond_to(:status)
+  end
+
+  it "has a phone" do
+    organization = Organization.new
+    expect(organization).to respond_to(:phone)
+  end
+
+  it "has an email" do
+    organization = Organization.new
+    expect(organization).to respond_to(:email)
+  end
+
+  it "has a description" do
+    organization = Organization.new
+    expect(organization).to respond_to(:description)
+  end
+
+  it "has a rejection_reason" do
+    organization = Organization.new
+    expect(organization).to respond_to(:rejection_reason)
+  end
+
+  it "has a liability_insurance" do
+    organization = Organization.new
+    expect(organization).to respond_to(:liability_insurance)
+  end
+
+  it "has a primary_name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:primary_name)
+  end
+
+  it "has a secondary_name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:secondary_name)
+  end
+
+  it "has a secondary_phone" do
+    organization = Organization.new
+    expect(organization).to respond_to(:secondary_phone)
+  end
+
+  it "has a title" do
+    organization = Organization.new
+    expect(organization).to respond_to(:title)
+  end
+
+  it "has a transportation" do
+    organization = Organization.new
+    expect(organization).to respond_to(:transportation)
+  end
+
 end
