a1d405256a22130354dfcd5497995790cbf50dd5
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-20 16:30:17 -0800

    ticket has a name, description, phone, and closed flag test passed, ticket closed flag default test passed

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,4 +6,29 @@ RSpec.describe Ticket, type: :model do
     Ticket.new
   end
 
+  it "has a name" do
+    ticket = Ticket.new
+    expect(ticket).to respond_to(:name)
+  end
+
+  it "has a description" do
+    ticket = Ticket.new
+    expect(ticket).to respond_to(:description)
+  end
+
+  it "has a phone" do
+    ticket = Ticket.new
+    expect(ticket).to respond_to(:phone)
+  end
+
+  it "has a closed flag" do
+    ticket = Ticket.new
+    expect(ticket).to respond_to(:closed)
+  end
+
+  it "closed defaults to false" do
+    ticket = Ticket.new
+    expect(ticket.closed).to eq(false)
+  end
+
 end
