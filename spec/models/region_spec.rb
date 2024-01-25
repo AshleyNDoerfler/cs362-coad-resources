e8baea0e24547d2c472745c26ef05ab122c36440
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-25 13:45:19 -0800

    region to_s method test passed

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -24,4 +24,12 @@ RSpec.describe Region, type: :model do
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
+
+  it "to_s returns the name" do
+    name = 'Mt. Hood'
+    region = Region.new(name: name)
+    result = region.to_s
+    expect(result).to eq(name)
+  end
+
 end
