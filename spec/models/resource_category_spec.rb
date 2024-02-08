8c30cda9d8fcd1ea58c7fa4a5c0984e2841c142c
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-08 11:07:59 -0800

    refactored resource category tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -6,16 +6,18 @@ RSpec.describe ResourceCategory, type: :model do
     ResourceCategory.new
   end
 
-  it "has a name" do
-    expect(ResourceCategory.new).to respond_to(:name)
-  end
+  describe "attributes" do
+    it "has a name" do
+      expect(ResourceCategory.new).to respond_to(:name)
+    end
 
-  it "has an active flag" do
-    expect(ResourceCategory.new).to respond_to(:active)
-  end
+    it "has an active flag" do
+      expect(ResourceCategory.new).to respond_to(:active)
+    end
 
-  it "is active by default" do
-    expect(ResourceCategory.new.active).to eq(true)
+    it "is active by default" do
+      expect(ResourceCategory.new.active).to eq(true)
+    end
   end
 
   describe "associations" do
@@ -28,34 +30,36 @@ RSpec.describe ResourceCategory, type: :model do
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
+  
+  describe "member functions" do
+    it "to_s returns the name" do
+      name = 'Peepee Poopoo'
+      resource_category = ResourceCategory.new(name: name)
+      result = resource_category.to_s
+      expect(result).to eq(name)
+    end
 
-  it "to_s returns the name" do
-    name = 'Peepee Poopoo'
-    resource_category = ResourceCategory.new(name: name)
-    result = resource_category.to_s
-    expect(result).to eq(name)
-  end
+    it "activate sets active to true" do
+      resource_category = ResourceCategory.new(active: false)
+      resource_category.activate
+      expect(resource_category.active).to eq(true)
+    end
 
-  it "activate sets active to true" do
-    resource_category = ResourceCategory.new(active: false)
-    resource_category.activate
-    expect(resource_category.active).to eq(true)
-  end
+    it "deactivate sets active to false" do
+      resource_category = ResourceCategory.new(active: true)
+      resource_category.deactivate
+      expect(resource_category.active).to eq(false)
+    end
 
-  it "deactivate sets active to false" do
-    resource_category = ResourceCategory.new(active: true)
-    resource_category.deactivate
-    expect(resource_category.active).to eq(false)
-  end
+    it "inactive? returns true if active is false" do
+      resource_category = ResourceCategory.new(active: false)
+      expect(resource_category.inactive?).to eq(true)
+    end
 
-  it "inactive? returns true if active is false" do
-    resource_category = ResourceCategory.new(active: false)
-    expect(resource_category.inactive?).to eq(true)
-  end
-
-  it "inactive? returns false if active is true" do
-    resource_category = ResourceCategory.new(active: true)
-    expect(resource_category.inactive?).to eq(false)
+    it "inactive? returns false if active is true" do
+      resource_category = ResourceCategory.new(active: true)
+      expect(resource_category.inactive?).to eq(false)
+    end
   end
 
 end
