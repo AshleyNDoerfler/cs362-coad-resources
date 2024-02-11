7f1dba9878d73019250fd27e70829201e14aee49
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-10 17:24:21 -0800

    Add factory bots in region_spec.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
+  let(:region) { build(:region, name: 'Mt. Hood') }
 
   it "exists" do
     Region.new
   end
 
   it "has a name" do
-    region = Region.new
     expect(region).to respond_to(:name)
   end
 
   it "has a string representation that is its name" do
     name = 'Mt. Hood'
-    region = Region.new(name: name)
     result = region.to_s
+    expect(result).to eq(name)
   end
 
   it { should have_many(:tickets) }
@@ -26,23 +26,23 @@ RSpec.describe Region, type: :model do
   end
 
   describe "member functions" do
+    let(:region) { build(:region, name: 'Mt. Hood') }
+    let(:region_unspecified) { build(:region, name: 'Unspecified') }
+
     it "to_s returns the name" do
       name = 'Mt. Hood'
-      region = Region.new(name: name)
       result = region.to_s
       expect(result).to eq(name)
     end
 
     it "unspecified returns the unspecified region" do
-      region = Region.unspecified
-      expect(region.name).to eq('Unspecified')
+      expect(region_unspecified.name).to eq('Unspecified')
     end
 
     it "unspecified returns the same region if it already exists" do
-      region = Region.new(name: 'Unspecified')
-      region.save
+      region_unspecified.save
       result = Region.unspecified
-      expect(result).to eq(region)
+      expect(result).to eq(region_unspecified)
     end
   end
 
