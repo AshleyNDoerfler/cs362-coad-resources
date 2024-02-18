3873892d0eeb2b300960154b7ce2b6613e2e52eb
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-17 18:36:16 -0800

    resource cat get indexed logged out test passed

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  let(:user) { create(:user, email: "email@gmail.com") } # Assuming you have a factory for users
+  let(:admin) { create(:admin) } # Assuming you have a factory for admins
+  let(:valid_attributes) { attributes_for(:resource_category) } # Assuming you have a factory for resource categories
+
+  describe 'GET #index while logged out' do
+    it { expect(get(:index)).to redirect_to(new_user_session_path) }
+  end
 
 end
