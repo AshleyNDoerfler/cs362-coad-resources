dab8cc42c0f2e3b0c13393a220c16f51f4422a12
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-19 14:37:58 -0800

    application helper test full title test passed

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  describe "test full title" do
+    it { expect(helper.full_title("")).to eq('Disaster Resource Network') }
+  end
 end
