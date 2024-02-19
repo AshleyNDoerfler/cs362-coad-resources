2a24f3f4ca9f8ff0e3d6edf9b8517663a6b55533
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-19 15:13:56 -0800

    org controller index test passed

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -4,8 +4,27 @@ RSpec.describe OrganizationsController, type: :controller do
 
 # NOTE: may need "Devise gem"
 
+describe "GET #index while logged in" do
+    let(:user) { create(:user, email: "email@gmail.com") }
+
+    before(:each) { sign_in(user) }
+
+    it { expect(get(:index)).to be_successful }
+  end
+
+  describe "GET #index while logged in as admin" do
+    let(:admin_user) { create(:user, :admin, email: "email@gmail.com") }
+
+    before(:each) { sign_in(admin_user) }
+
+    it { expect(get(:index)).to be_successful }
+  end
+
+  describe "GET #index while logged out" do
+    it { expect(get(:index)).to redirect_to(new_user_session_path) }
+  end
+
 # TODO
-# index
 # new
 # create
 # edit
