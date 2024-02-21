2be945d3910b2524f33582b72a6a927fe9bc57af
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-21 14:42:08 -0800

    refion controller create tests passed

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,6 +2,40 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  context 'as an admin user' do
+    let(:admin) { create(:user, :admin, email: "email@gmail.com") }
+    before(:each) { sign_in(admin) }
+
+    describe 'POST #create' do
+      let(:region) { create(:region) }
+
+      context 'success' do
+        let(:params) do
+          {
+            region: {
+              name: 'Fake Region',
+              description: 'Fake Description'
+            }
+          }
+        end
+
+        specify { expect(post(:create, params: params)).to redirect_to regions_path }
+      end
+
+      context 'failure' do
+        let(:params) do
+          {
+            region: {
+              name: nil,
+              description: nil
+            }
+          }
+        end
+
+        specify { expect(post(:create, params: params)).to be_successful }
+      end
+    end
+  end
   # TODO
   # index
   # show
@@ -10,5 +44,5 @@ RSpec.describe RegionsController, type: :controller do
   # edit
   # update
   # destroy
-  
+
 end
