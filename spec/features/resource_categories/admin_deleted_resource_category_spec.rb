056c65f3e9aa854451e287babc0534be86c113ce
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-08 12:57:34 -0800

    commented out test

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Deleting a Resource Category', type: :feature do
+# RSpec.describe 'Deleting a Resource Category', type: :feature do
+#   let(:admin) { create(:admin) }
+#   let!(:resource_category) { create(:resource_category) }
 
-end
+#   context 'as an admin' do
+#     before do
+#       log_in_as(admin)
+#       visit resource_categories_path
+#     end
+
+#     it 'deletes a resource category' do
+#       click_on 'Delete'
+#       expect(page).to have_content('Resource Category was successfully deleted')
+#     end
+#   end
+
+# end
