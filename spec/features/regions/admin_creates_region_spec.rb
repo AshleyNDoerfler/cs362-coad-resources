345f2d33f05fabe0eedc949bdde54fde81d45594
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-13 14:53:40 -0700

    feature test passed

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+  let(:admin) { create(:user, :admin, email: "email@gmail.com") }
+  let(:region) { build(:region) }
 
+  before { log_in_as(admin) }
+
+  context 'as an admin' do
+    it 'creates a region' do
+      visit regions_path
+      click_on 'Add Region'
+      fill_in 'Name', with: region.name
+      click_on 'Add Region'
+      expect(page).to have_content(region.name)
+    end
+  end
 end
