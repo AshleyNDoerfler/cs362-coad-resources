b5122f4829c5489a9ad8326e0c65f93d47e6e374
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-15 13:06:00 -0700

    user login test passed

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  let(:user) { create(:user, email: "user@gmail.com") }
+
+  it 'logs in a user' do
+    visit new_user_session_path  
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    expect(page).to have_content 'Signed in successfully'  
+    expect(page).to have_content "Welcome to your dashboard!"
+  end
 
 end
