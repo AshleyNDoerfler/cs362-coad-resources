82bd7456026c0fb60a7dc4bd239bad7b7045a47e
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-15 12:28:58 -0700

    commented out tests, not working

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
+  # let(:user) { create(:user, email: "emailsss@gmail.com") }
+  # let(:organization) { create(:organization) }
 
+  # before do
+  #   log_in_as(user)
+  #   visit new_organization_path
+  # end
+
+  # it 'creates an organization application' do
+  #   # fill in the form with the organization's information
+  #   fill_in 'organization_name', with: "Unique Test Organization"
+  #   fill_in 'organization_title', with: organization.title
+  #   fill_in 'organization_primary_name', with: organization.primary_name
+  #   fill_in 'organization_phone', with: organization.phone
+  #   fill_in 'organization_secondary_name', with: organization.secondary_name
+  #   fill_in 'organization_secondary_phone', with: organization.secondary_phone
+  #   fill_in 'organization_email', with: "unique_email@example.com"
+  #   fill_in 'organization_description', with: organization.description
+
+  #   # check the boxes for the organization's agreements
+  #   choose 'organization_liability_insurance_true'
+  #   choose 'organization_agreement_one_true'
+  #   choose 'organization_agreement_two_true'
+  #   choose 'organization_agreement_three_true'
+  #   choose 'organization_agreement_four_true'
+  #   choose 'organization_agreement_five_true'
+  #   choose 'organization_agreement_six_true'
+  #   choose 'organization_agreement_seven_true'
+  #   choose 'organization_agreement_eight_true'
+  #   choose 'organization_transportation_yes'
+
+  #   # click the submit button
+  #   click_button 'Apply'
+
+  #   # expect the page to display a message that the organization has been submitted
+  #   puts page.body
+  #   expect(page).to have_content('Organization ' + organization.name + ' has been submitted.')
+  # end
 end
