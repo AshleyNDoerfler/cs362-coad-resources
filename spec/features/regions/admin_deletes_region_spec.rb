0c7a4ee8d10014fdea9433a4d66c715c2ad85be1
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-15 12:51:42 -0700

    admin deletes region test passed

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  let(:admin) { create(:user, :admin, email: "email@gmail.com") }
+  let(:region) { create(:region) }
 
+  before do
+    log_in_as(admin)
+    visit region_path(region)
+  end
+
+  it 'deletes the region' do
+    click_on 'Delete'
+    expect(page).to have_content('Region ' + region.name + ' was deleted.')
+  end
 end
