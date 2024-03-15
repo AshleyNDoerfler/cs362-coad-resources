484e2b1b19baa9a0a4347778c03eef9aa7d4060c
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-15 10:31:47 -0700

    feature test for approving organization passed

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  let(:admin) { create(:user, :admin, email: "email@gmail.com") }
+  let(:organization) { create(:organization) }
+
+  before do
+    log_in_as(admin)
+    organization.set_default_status
+    visit organizations_path
+  end
+
+  it 'approves an organization' do
+    click_on 'Pending'
+    click_on organization.name
+    click_on 'Approve'
+    expect(page).to have_content('Organization ' + organization.name + ' has been approved.')
+    expect(page).to have_content(organization.name)
+    expect(page).to have_content('Approved')
+  end
 
 end
