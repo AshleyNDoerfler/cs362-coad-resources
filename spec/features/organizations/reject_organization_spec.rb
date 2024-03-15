bb175e16b216d1256aa319a24527932334db9586
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-15 12:31:03 -0700

    reject organization test passed

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+  let(:admin) { create(:user, :admin, email: "email@gmail.com") }
+  let(:organization) { create(:organization) }
 
+  before do
+    log_in_as(admin)
+    organization.set_default_status
+    visit organizations_path
+  end
+
+  it 'approves an organization' do
+    click_on 'Pending'
+    click_on organization.name
+    click_on 'Reject'
+    expect(page).to have_content('Organization ' + organization.name + ' has been rejected.')
+    expect(page).to have_content(organization.name)
+    expect(page).to have_content('Approved')
+  end
 end
